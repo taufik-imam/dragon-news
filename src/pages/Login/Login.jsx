2f33b8d5ab6801d5f3515d047fb102ad546a535a
taufik-imam <taufikimamhridoy@gmail.com>
2023-10-06 02:48:16 +0600

    added projects

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This Login Pages</h2>
+            
+        </div>
+    );
+};
+
+export default Login;
